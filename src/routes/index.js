87a8ff701af205456cd0ce67c4091117ab780c1f
Jorge Diaz <jorjorf@gmail.com>
2022-12-06 17:32:53 -0400

    Implementar y probar las conexiones

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import Home from '../pages/Home';
 import Character from '../pages/Character';
 import Error404 from '../pages/Error404';
 import getHash from '../utils/getHash';
-import resolveRoutes from '../utils/resolveRoute';
+import resolveRoutes from '../utils/resolveRoutes';
 
 const routes = {
   '/': Home,
@@ -16,6 +16,10 @@ const router = async () => {
   const content = null || document.getElementById('content');
 
   header.innerHTML = await Header();
+  let hash = getHash();
+  let route = await resolveRoutes(hash);
+  let render = routes[route] ? routes[route] : Error404;
+  content.innerHTML = await render();
 };
 
 export default router;
